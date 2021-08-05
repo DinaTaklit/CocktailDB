b2ad637b237addbcc1f83eb92b45af55fb8cee5b
DinaTaklit <ez_taklit@esi.dz>
2021-08-05 04:28:28 +0100

    feat: component: SearchForm: static implementation

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,9 +3,18 @@ import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input 
+            type="text" 
+            id="name"
+            name="name"
+          />
+        </div>
+      </form>  
+    </section>
   )
 }
 
