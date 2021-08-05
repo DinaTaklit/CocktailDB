2891131639a36c406d948d2ff5c1728d88ceea8a
DinaTaklit <ez_taklit@esi.dz>
2021-08-05 04:40:32 +0100

    feat: context: add state and fetchData logic

--- src/context.js
+++ src/context.js
@@ -5,8 +5,68 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({children}) => {
+
+    // State to set up loading
+    const [loading, setLoading] = useState(true)
+    // State to set up searchTerm
+    const [searchTerm, setSearchTerm] = useState('a')
+    // State to store the coktails list
+    const [coktails, setCotails] = useState([])
+
+    // Function that fetchCocktails
+    const fetchCocktails = async() => {
+        setLoading(true)
+        try {
+            // Fetch cocktails from CocktailsDB API
+            const {coktails} = await fetch(url + searchTerm).then(response => response.json())
+            const {drinks} = coktails // Get the drinks from the fetched data
+            // If the drinks are not empty, set the state to the list of drinks
+            if (drinks) {
+                // Get the useful items from the drinks
+                const newCocktails = drinks.map((item) => {
+                    const {
+                      idDrink,
+                      strDrink,
+                      strDrinkThumb,
+                      strAlcoholic,
+                      strGlass,
+                    } = item
+          
+                    return {
+                      id: idDrink,
+                      name: strDrink,
+                      image: strDrinkThumb,
+                      info: strAlcoholic,
+                      glass: strGlass,
+                    }
+                })
+                setCotails(newCocktails)
+            } else {
+                setCotails([])
+            }
+
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+    // UseEffect that run fetchCocktails when the component mounts or when there is a change in the searchTerm
+
+    useEffect(() => {
+        fetchCocktails()
+    }, [searchTerm])
+
     return (
-        <AppContext.Provider value={'hello'}>
+        <AppContext.Provider 
+            value={{
+                loading,
+                searchTerm,
+                coktails,
+                setSearchTerm
+            }}
+        >
             {children}
         </AppContext.Provider>
     )
