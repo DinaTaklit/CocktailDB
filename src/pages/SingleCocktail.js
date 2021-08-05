75ba7092df8c54f4507e1f5e3153c44e48195d53
DinaTaklit <ez_taklit@esi.dz>
2021-08-05 06:34:25 +0100

    feat: componenet: SingleCocktail: fetch the cocktail with the given id

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Loading from '../components/Loading'
 import { useParams, Link } from 'react-router-dom'
+
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
@@ -13,6 +14,57 @@ const SingleCocktail = () => {
 
   // Cocktail state to store the cocktail item
   const [cocktail, setCocktail] = useState(null)
+
+  // UseEffect to fetch the cocktail item
+  useEffect(() => {
+    setLoading(true)
+    async function getCocktail(){
+      try{
+        const cocktail = await fetch(`${url}${id}`).then(response => response.json())
+        if (cocktail.drinks){
+          // Get the keys and set useful name
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = cocktail.drinks[0]
+          // List of ingredients
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ]
+          // Create new Cocktail object
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          }
+          setCocktail(newCocktail)
+        } else {
+          setCocktail(null)
+        }
+      }catch(error){
+        console.log(error)
+      }
+      setLoading(false)
+    }
+    getCocktail()
+  }, [id])
 
   // Laoading 
   if (loading){
@@ -48,30 +100,30 @@ const SingleCocktail = () => {
           </h2>
           <div className="drink">
             <img src={image} alt={name} />
-          </div>
-          <div className='drink-info'>
-            <p>
-              <span className='drink-data'>name :</span> {name}
-            </p>
-            <p>
-              <span className='drink-data'>category :</span> {category}
-            </p>
-            <p>
-              <span className='drink-data'>info :</span> {info}
-            </p>
-            <p>
-              <span className='drink-data'>glass :</span> {glass}
-            </p>
-            <p>
-              <span className='drink-data'>instructons :</span> {instructions}
-            </p>
-            <p>
-              <span className='drink-data'>ingredients :</span>
-              {ingredients.map((item, index) => {
-                return item ? <span key={index}> {item}</span> : null
-              })}
-            </p>
-          </div>
+            <div className='drink-info'>
+              <p>
+                <span className='drink-data'>name :</span> {name}
+              </p>
+              <p>
+                <span className='drink-data'>category :</span> {category}
+              </p>
+              <p>
+                <span className='drink-data'>info :</span> {info}
+              </p>
+              <p>
+                <span className='drink-data'>glass :</span> {glass}
+              </p>
+              <p>
+                <span className='drink-data'>instructons :</span> {instructions}
+              </p>
+              <p>
+                <span className='drink-data'>ingredients :</span>
+                {ingredients.map((item, index) => {
+                  return item ? <span key={index}> {item}</span> : null
+                })}
+              </p>
+            </div>
+            </div>
         </section>
       </div>
     )
