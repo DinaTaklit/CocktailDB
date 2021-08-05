3673e16c1b75a478a19a078db0c3c25291d98d40
DinaTaklit <ez_taklit@esi.dz>
2021-08-05 04:51:40 +0100

    feat: componenet: CocktailList: implementation

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,10 +4,36 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+
+  // Get global states
+  const { cocktails, loading } = useGlobalContext()
+
+  // If cocktails are loading, show loading
+  if (loading) {
+    return <Loading />
+  }
+
+  // If the list is empty, show a message
+  if (cocktails.length < 1) {
+    <section className="section">
+      <h2 className="section-title">
+        No Cocktails Found
+      </h2>
+    </section>
+  }
+
+  // Otherwise, show cocktail list
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className="section">
+      <h2 className="section-title">
+        Cocktails
+      </h2>
+      <div className="cocktails-center">
+        {cocktails.map(cocktail => {
+          return <Cocktail key={cocktail.id} {...cocktail} />
+        })}
+      </div>
+    </section>
   )
 }
 
