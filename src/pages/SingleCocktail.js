7130d535151178987bf63dc428073f39013ddd74
DinaTaklit <ez_taklit@esi.dz>
2021-08-05 06:23:59 +0100

    feat: componenet: SingleCocktail: implementation

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,14 +1,81 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Loading from '../components/Loading'
 import { useParams, Link } from 'react-router-dom'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
-  return (
-    <div>
-      <h2>single cocktail page </h2>
-    </div>
-  )
+
+  // Get the cocktail id from the URL
+  const { id } = useParams()
+
+  // Loading state while searching for the cocktail item
+  const [loading, setLoading] = useState(false)
+
+  // Cocktail state to store the cocktail item
+  const [cocktail, setCocktail] = useState(null)
+
+  // Laoading 
+  if (loading){
+    return <Loading />
+  }
+  // if the cocktail is not found, display a message
+  if (!cocktail){
+    return (
+      <section className="section">
+        <h2 className="section-title">Cocktail not found</h2>
+      </section>
+    )
+  } else {
+    // if the cocktail is not null, then render it
+
+    // Destructure the keys from the cocktail
+    const {
+      name,
+      image,
+      category,
+      info,
+      glass,
+      instructions,
+      ingredients,
+    } = cocktail
+
+    return (
+      <div>
+        <section className="section cocktail-section"> 
+          <Link to={`/`} className="btn btn-primary">Back home</Link>
+          <h2 className="section-title">
+            {name}
+          </h2>
+          <div className="drink">
+            <img src={image} alt={name} />
+          </div>
+          <div className='drink-info'>
+            <p>
+              <span className='drink-data'>name :</span> {name}
+            </p>
+            <p>
+              <span className='drink-data'>category :</span> {category}
+            </p>
+            <p>
+              <span className='drink-data'>info :</span> {info}
+            </p>
+            <p>
+              <span className='drink-data'>glass :</span> {glass}
+            </p>
+            <p>
+              <span className='drink-data'>instructons :</span> {instructions}
+            </p>
+            <p>
+              <span className='drink-data'>ingredients :</span>
+              {ingredients.map((item, index) => {
+                return item ? <span key={index}> {item}</span> : null
+              })}
+            </p>
+          </div>
+        </section>
+      </div>
+    )
+  } 
 }
 
 export default SingleCocktail
