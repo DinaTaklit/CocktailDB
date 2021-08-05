2c12fef2dbb9f58bd247ef8c721796c42cf6d7ca
DinaTaklit <ez_taklit@esi.dz>
2021-08-05 04:04:49 +0100

    feat: page: Error: implement Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>oops! it 's dead end</h1>
+        <Link to="/">Go back to home page</Link>
+      </div>
+    </section>
   )
 }
 
